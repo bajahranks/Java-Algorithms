b9f30e4f7a97daff7775033ad88338d2a17404ee
Ravi Lamontagne <bajahranks@users.noreply.github.com>
2018-04-14 03:49:06 -0400

    Update DisplayThreeMessages.java

--- DisplayThreeMessages.java
+++ DisplayThreeMessages.java
@@ -1,9 +1,11 @@
-/*
+/**
 * Write a program that displays Welcome to Java,
 * Welcome to Computer Science, and Programming is fun.
+*
+* @author Ravi Lamontagne
 */
 
-/* @author Ravi Lamontagne */ 
+ 
 
 public class DisplayThreeMessages {
     public static void main (String[] args){
