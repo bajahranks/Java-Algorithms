e05010466c043ad89e476a1ebf8c42983bd4671f
Ravi Lamontagne <lamontagne.ravi@gmail.com>
2018-04-14 03:25:38 -0400

    Revert "Exercises from Liang E10"
    This reverts commit bd9287362f1417bb19b76282de4d199ce9702bd0.

--- DisplayThreeMessages.java
+++
@@ -1,7 +0,0 @@
-public class DisplayThreeMessages {
-    public static void main (String[] args){
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Computer Science");
-        System.out.println("Welcome to Programming is fun");
-    }
-}
--- TwoByTwoLinearEquations.java
+++
@@ -1,9 +0,0 @@
-public class TwoByTwoLinearEquations {
-    public static void main (String[] args){
-        double solveX = ((44.5 * 55) - (50.2 * 5.9)) / ((3.4 * 55) - (50.2 * 2.1));
-        double solveY = ((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 55) - (50.2 * 2.1));
-
-        System.out.println("X is: " + solveX);
-        System.out.println("Y is: " + solveY);
-    }
-}
