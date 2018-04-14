750fa9e04984d06af5354f20e8435cbdfc7040b8
Ravi Lamontagne <bajahranks@users.noreply.github.com>
2018-04-14 03:47:37 -0400

    Update DisplayThreeMessages.java

--- DisplayThreeMessages.java
+++ DisplayThreeMessages.java
@@ -1,3 +1,10 @@
+/*
+* Write a program that displays Welcome to Java,
+* Welcome to Computer Science, and Programming is fun.
+*/
+
+/* @author Ravi Lamontagne */ 
+
 public class DisplayThreeMessages {
     public static void main (String[] args){
         System.out.println("Welcome to Java");
