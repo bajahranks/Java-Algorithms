92d2a56978e247cf9ef776acb0b596ef3722e8c7
Ravi Lamontagne <bajahranks@users.noreply.github.com>
2018-04-14 04:10:09 -0400

    Update TwoByTwoLinearEquations.java

--- TwoByTwoLinearEquations.java
+++ TwoByTwoLinearEquations.java
@@ -1,3 +1,16 @@
+/**
+* You can use Cramer’s rule to solve the following 2 * 2 system of linear equation:
+* ax + by = e
+* cx + dy = f
+* x = ed - bf/ad - bc
+* y = af - ec/ad - bc
+* Write a program that solves the following equation and displays the value for x and y:
+* 3.4x + 50.2y = 44.5
+* 2.1x + .55y = 5.9
+*
+* @author Ravi Lamontagne
+*/
+
 public class TwoByTwoLinearEquations {
     public static void main (String[] args){
         double solveX = ((44.5 * 55) - (50.2 * 5.9)) / ((3.4 * 55) - (50.2 * 2.1));
